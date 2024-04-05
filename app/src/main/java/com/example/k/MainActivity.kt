891d6d4f0db7c4ec2770694f7e2f6fe5df3faf7f
skveroo <101058520+skveroo@users.noreply.github.com>
2024-04-05 19:00:04 +0200

    Upgraded side menu,added bottom bar with buttons, added a dev button for instant login

--- app/src/main/java/com/example/k/MainActivity.kt
+++ app/src/main/java/com/example/k/MainActivity.kt
@@ -2,26 +2,31 @@ package com.example.k
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.k.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 import android.widget.Toast
-import com.example.k.databinding.ActivityMainBinding
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var logoutButton: Button
     private lateinit var binding : ActivityMainBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
 
         firebaseAuth = FirebaseAuth.getInstance()
@@ -32,14 +37,47 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
         }
-        binding.SideMenuButton?.setOnClickListener{
-            val intent = Intent(this,SideMenu::class.java)
+
+        binding.AccountButton?.setOnClickListener{
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+        binding.CalendarV?.setOnClickListener{
+            val intent = Intent(this, CalendarView::class.java)
             startActivity(intent)
+        }
+
+        drawerLayout = findViewById(R.id.my_drawer_layout)
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close)
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val navigationView: NavigationView = findViewById(R.id.navigation_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // Handle navigation view item clicks here
+            when (menuItem.itemId) {
+                R.id.nav_preferences -> {
+                    // Handle item 1 click
+                    // For example, start a new activity
+                    val intent = Intent(this, PrefChanging::class.java)
+                    startActivity(intent)
+                }
+                // Add more cases for other menu items if needed
+            }
+            // Close the drawer after handling the click
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
         }
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            true
+        } else super.onOptionsItemSelected(item)
+    }
 
     private fun logout() {
         firebaseAuth.signOut()
