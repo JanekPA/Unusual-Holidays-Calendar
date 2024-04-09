92d27beb48698296ebcd7be0d3e1537b9a1e2dbb
skveroo <101058520+skveroo@users.noreply.github.com>
2024-04-09 22:39:08 +0200

    Upgraded side menu,added bottom bar with buttons, added a dev button for instant login

---
+++ app/src/main/java/com/example/k/Profile.kt
@@ -0,0 +1,12 @@
+package com.example.k
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Profile : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.profile) // assuming your XML file is named profile.xml
+    }
+}
