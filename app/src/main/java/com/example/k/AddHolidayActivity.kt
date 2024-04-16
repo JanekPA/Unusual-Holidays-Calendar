b78d01b7f95ecf489542b230e865fb9bc7698d1f
BartZaj <zajaczek5155@gmail.com>
2024-04-16 23:21:47 +0200

    AddPref

--- app/src/main/java/com/example/k/AddHolidayActivity.kt
+++ app/src/main/java/com/example/k/AddHolidayActivity.kt
@@ -1,9 +1,12 @@
 package com.example.k
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.CalendarView
 import android.widget.EditText
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.FirebaseDatabase
@@ -12,7 +15,9 @@ class AddHolidayActivity : AppCompatActivity() {
 
     private lateinit var calendarView: CalendarView
     private lateinit var holidayNameEditText: EditText
-    private fun Int.pad(digits: Int) = toString().padStart(digits, '0')
+    private lateinit var countryAutoComplete: AutoCompleteTextView
+    private lateinit var hobbySpinner: Spinner
+    private lateinit var activitySpinner: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,32 +25,60 @@ class AddHolidayActivity : AppCompatActivity() {
 
         calendarView = findViewById(R.id.calendarV)
         holidayNameEditText = findViewById(R.id.holidayNameEditText)
-        val saveButton: Button = findViewById(R.id.saveButton)
+        countryAutoComplete = findViewById(R.id.countryAutoComplete)
+        hobbySpinner = findViewById(R.id.hobbySpinner)
+        activitySpinner = findViewById(R.id.activitySpinner)
 
-        var selectedDate = ""
+        val countriesAdapter = ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_dropdown_item_1line,
+            resources.getStringArray(R.array.countries)
+        )
+        countryAutoComplete.setAdapter(countriesAdapter)
 
-        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
-            val formattedDate = "${dayOfMonth.pad(2)}-${(month + 1).pad(2)}"
-            selectedDate = formattedDate
+        var selectedDate =
+            ""  // Inicjalizacja zmiennej poza listenerem, aby była dostępna globalnie w klasie
+
+        calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            // Formatuj datę do formatu DD-MM
+            selectedDate = "${dayOfMonth.toString().padStart(2, '0')}-${
+                (month + 1).toString().padStart(2, '0')
+            }"
         }
 
+        val saveButton: Button = findViewById(R.id.saveButton)
         saveButton.setOnClickListener {
-            val holidayName = holidayNameEditText.text.toString().trim()
+            saveHolidayDetails(selectedDate)  // Przekazuj wybraną datę do funkcji zapisującej
+        }
+    }
 
-            if (holidayName.isEmpty()) {
-                Toast.makeText(this, "Holiday name cannot be empty!", Toast.LENGTH_LONG).show()
-            } else if (selectedDate.isEmpty()) {
-                Toast.makeText(this, "Please select a date!", Toast.LENGTH_LONG).show()
-            } else {
-                val database = FirebaseDatabase.getInstance()
-                val holidaysRef = database.getReference("HolidayNames")
-                holidaysRef.child(selectedDate).setValue(holidayName).addOnCompleteListener {
-                    if (it.isSuccessful) {
-                        Toast.makeText(this, "Holiday saved!", Toast.LENGTH_SHORT).show()
-                        finish()
-                    } else {
-                        Toast.makeText(this, "Write error!", Toast.LENGTH_SHORT).show()
-                    }
+    private fun saveHolidayDetails(selectedDate: String) {
+        val holidayName = holidayNameEditText.text.toString().trim()
+        val selectedCountry = countryAutoComplete.text.toString()
+        val selectedHobbies = hobbySpinner.selectedItem.toString()
+        val selectedActivities = activitySpinner.selectedItem.toString()
+
+        if (holidayName.isEmpty()) {
+            Toast.makeText(this, "Holiday name cannot be empty!", Toast.LENGTH_LONG).show()
+        } else if (selectedDate.isEmpty()) {
+            Toast.makeText(this, "Please select a date!", Toast.LENGTH_LONG).show()
+        } else {
+            val database = FirebaseDatabase.getInstance()
+            val holidayDetailsRef = database.getReference("HolidaysDetails").child(selectedDate)
+
+            val holidayDetails = mapOf(
+                "name" to holidayName,
+                "country" to selectedCountry,
+                "hobbies" to selectedHobbies,
+                "activities" to selectedActivities
+            )
+
+            holidayDetailsRef.setValue(holidayDetails).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Toast.makeText(this, "Holiday details saved!", Toast.LENGTH_SHORT).show()
+                    finish()
+                } else {
+                    Toast.makeText(this, "Write error!", Toast.LENGTH_SHORT).show()
                 }
             }
         }
--- app/src/main/java/com/example/k/CalendarView.kt
+++ app/src/main/java/com/example/k/CalendarView.kt
@@ -22,27 +22,27 @@ class CalendarView : AppCompatActivity() {
         setContentView(binding.root)
 
         dateTV = binding.idTVDate
-        holidayView=binding.idHolidayView
+        holidayView = binding.idHolidayView
         calendarView = binding.calendarView
 
         val database = FirebaseDatabase.getInstance()
-        val holidaysRef = database.getReference("HolidayNames")
+        val holidaysDetailsRef = database.getReference("HolidaysDetails")
 
         val addHolidayButton: Button = findViewById(R.id.addHolidayButton)
         addHolidayButton.setOnClickListener {
-            val intent = Intent(this, AddHolidayActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, AddHolidayActivity::class.java))
         }
 
         val editHolidayButton: Button = findViewById(R.id.editHolidayButton)
         editHolidayButton.setOnClickListener {
             val dateKey = dateTV.text.toString().substring(0, 5) // Odczytaj klucz "DD-MM"
-            holidaysRef.child(dateKey).get().addOnSuccessListener { dataSnapshot ->
+            holidaysDetailsRef.child(dateKey).get().addOnSuccessListener { dataSnapshot ->
                 if (dataSnapshot.exists()) {
-                    val holidayName = dataSnapshot.getValue(String::class.java)
-                    val intent = Intent(this, EditHolidayActivity::class.java)
-                    intent.putExtra("date", dateKey)
-                    intent.putExtra("holidayName", holidayName)
+                    val details = dataSnapshot.value as Map<String, Any>
+                    val intent = Intent(this, EditHolidayActivity::class.java).apply {
+                        putExtra("date", dateKey)
+                        putExtra("holidayDetails", HashMap(details))
+                    }
                     startActivity(intent)
                 } else {
                     Toast.makeText(this, "W tym dniu nie ma święta do edycji!", Toast.LENGTH_LONG).show()
@@ -50,18 +50,16 @@ class CalendarView : AppCompatActivity() {
             }
         }
 
-        fun Int.pad(digits: Int) = this.toString().padStart(digits, '0')
+        fun Int.pad(digits: Int) = toString().padStart(digits, '0')
         calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
             val dateKey = "${dayOfMonth.pad(2)}-${(month + 1).pad(2)}"
-            val fullDate = "$dateKey-$year"
-
-            holidaysRef.child(dateKey).get().addOnSuccessListener { dataSnapshot ->
+            holidaysDetailsRef.child(dateKey).get().addOnSuccessListener { dataSnapshot ->
                 if (dataSnapshot.exists()) {
-                    val holidayName = dataSnapshot.getValue(String::class.java)
-                    dateTV.text = fullDate
-                    holidayView.text = holidayName ?: "Holiday but no name found"
+                    val details = dataSnapshot.value as Map<String, String>
+                    dateTV.text = "$dateKey-$year"
+                    holidayView.text = details["name"] ?: "Holiday but no name found"
                 } else {
-                    dateTV.text = fullDate
+                    dateTV.text = "$dateKey-$year"
                     holidayView.text = "No holiday"
                 }
             }
