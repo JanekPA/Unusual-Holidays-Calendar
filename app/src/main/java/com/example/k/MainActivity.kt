5d4876991becd5736357a0240e28a925b18d1f3e
skveroo <101058520+skveroo@users.noreply.github.com>
2024-04-17 19:33:31 +0200

    Added: - Adding profile picture in profile tab - Visible profile picture in profile and nav_menu - Added popup asking for permissions to use phone gallery - Added better background in profile tab Fixed: - Wrong color of a account drawable in registration - Fixed placement constraints in account tab

--- app/src/main/java/com/example/k/MainActivity.kt
+++ app/src/main/java/com/example/k/MainActivity.kt
@@ -8,18 +8,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.k.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
+import android.widget.ImageView
+import com.google.firebase.auth.FirebaseUser
+import com.bumptech.glide.Glide
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.util.Log
+import com.google.firebase.storage.FirebaseStorage
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+    private lateinit var profileImageView: ImageView
+    private val REQUEST_EXTERNAL_STORAGE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,33 +40,40 @@ class MainActivity : AppCompatActivity() {
         firebaseAuth = FirebaseAuth.getInstance()
         setupViews()
 
-
         val navigationView: NavigationView = findViewById(R.id.navigation_view)
+        val headerView = navigationView.getHeaderView(0)
+
+        profileImageView = headerView.findViewById(R.id.View_Image2)
+
+
+
         navigationView.setNavigationItemSelectedListener { menuItem ->
 
             when (menuItem.itemId) {
                 R.id.nav_preferences -> {
-
                     val intent = Intent(this, PrefChanging::class.java)
                     startActivity(intent)
                 }
                 R.id.nav_profile -> {
-
                     val intent = Intent(this, Profile::class.java)
                     startActivity(intent)
                 }
                 R.id.nav_settings -> {
-
                     val intent = Intent(this, Settings::class.java)
                     startActivity(intent)
                 }
                 R.id.Logout_button -> logout()
-
             }
 
             drawerLayout.closeDrawer(GravityCompat.START)
             true
         }
+
+
+        if (!checkExternalStoragePermission()) {
+            requestExternalStoragePermission()
+        }
+        loadProfilePicture()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -66,12 +83,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupViews() {
-        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
         binding.AccountButton?.setOnClickListener {
             drawerLayout.openDrawer(GravityCompat.START)
         }
@@ -94,11 +105,54 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, SignInActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         startActivity(intent)
-        val preferences = getSharedPreferences("checkbox",MODE_PRIVATE)
+        val preferences = getSharedPreferences("checkbox", MODE_PRIVATE)
         val editor = preferences.edit()
-        editor.putString("remember","false")
+        editor.putString("remember", "false")
         editor.apply()
         finish()
         Toast.makeText(this, "Successfully logged out", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        loadProfilePicture()
+    }
+    private fun loadProfilePicture() {
+        // How is this working? idk trust me bro
+        // Code bellow made especially for nav_drawer
+        val currentUser: FirebaseUser? = firebaseAuth.currentUser
+        currentUser?.let {
+            val uid = it.uid
+            val profilePicturesRef = FirebaseStorage.getInstance().reference.child("ProfilePictures/$uid/")
+            val filename = "profile.jpg"
+            val imageRef = profilePicturesRef.child(filename)
+
+            imageRef.downloadUrl.addOnSuccessListener { uri ->
+                Glide.with(this@MainActivity)
+                    .load(uri)
+                    .into(profileImageView)
+            }.addOnFailureListener { exception ->
+                // errors
+                Log.e("MainActivity", "Error loading profile picture from Firebase Storage: $exception")
+            }
+        }
+    }
+
+    private fun checkExternalStoragePermission(): Boolean {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+    }
+    private fun requestExternalStoragePermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_EXTERNAL_STORAGE)
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_EXTERNAL_STORAGE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted
+            } else {
+                // Permission denied
+                Toast.makeText(this, "Permission denied. Unable to access external storage.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
