7957ea9946647c556bb081e0486fab30069a200d
BartZaj <zajaczek5155@gmail.com>
2024-04-19 19:03:44 +0200

    AddPreferences

--- app/src/main/java/com/example/k/AddHolidayActivity.kt
+++ app/src/main/java/com/example/k/AddHolidayActivity.kt
@@ -1,44 +1,120 @@
 package com.example.k
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.CalendarView
 import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.k.databinding.ActivityAddHolidayBinding
+import com.example.k.models.ListItem
+import com.example.k.models.MultiSelectSpinnerAdapter
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
 class AddHolidayActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityAddHolidayBinding
     private lateinit var calendarView: CalendarView
     private lateinit var holidayNameEditText: EditText
     private lateinit var countryAutoComplete: AutoCompleteTextView
-    private lateinit var hobbiesButton: Button
-    private lateinit var activitiesButton: Button
-
-    private var selectedHobbies = mutableListOf<String>()
-    private var selectedActivities = mutableListOf<String>()
+    private var selectedActivity: MutableList<ListItem>? = mutableListOf()
+    private var spinnerActivityListItem: ArrayList<ListItem>? = ArrayList()
+    private var selectedHobby: MutableList<ListItem> = mutableListOf()
+    private var spinnerHobbyListItem: ArrayList<ListItem>? = ArrayList()
+    private var spinnerActivity: Spinner? = null
+    private var spinnerHobby: Spinner? = null
+    private var nameActivity: TextView? = null
+    private var nameHobby: TextView? = null
     private lateinit var selectedDate: String
+    private lateinit var firebaseRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_holiday)
+        enableEdgeToEdge()
+        setContentView(binding.root)
 
         calendarView = findViewById(R.id.calendarV)
         holidayNameEditText = findViewById(R.id.holidayNameEditText)
         countryAutoComplete = findViewById(R.id.countryAutoComplete)
-        hobbiesButton = findViewById(R.id.hobbiesButton)
-        activitiesButton = findViewById(R.id.activitiesButton)
+        spinnerActivity = findViewById(R.id.activitySpinner)
+        spinnerHobby = findViewById(R.id.hobbySpinner)
+        nameActivity = findViewById(R.id.activityHead)
+        nameHobby = findViewById(R.id.hobbyHead)
 
-        val countriesAdapter = ArrayAdapter(
+        val activitiesArray = resources.getStringArray(R.array.activities)
+        for (activity in activitiesArray) {
+            spinnerActivityListItem?.add(ListItem(activity))
+        }
+
+        selectedActivity!!.clear()
+
+        val hobbyArray = resources.getStringArray(R.array.hobbys)
+        for (hobby in hobbyArray) {
+            spinnerHobbyListItem?.add(ListItem(hobby))
+        }
+
+        selectedHobby.clear()
+
+        val adapter = MultiSelectSpinnerAdapter(
             this,
-            android.R.layout.simple_dropdown_item_1line,
-            resources.getStringArray(R.array.countries)
+            spinnerActivityListItem!!,
+            selectedActivity!!
         )
-        countryAutoComplete.setAdapter(countriesAdapter)
+
+        spinnerActivity?.adapter = adapter
+
+        adapter.setOnItemSelectedListener(object :
+            MultiSelectSpinnerAdapter.OnItemSelectedListener {
+            override fun onItemSelected(
+                selectedItems: List<ListItem>,
+                pos: Int,
+            ) {
+                nameActivity?.text = "Activity"
+                Log.e("getSelectedItems", selectedItems.toString())
+                Log.e("getSelectedItems", selectedItems.size.toString())
+            }
+        }
+        )
+
+        val adapter2 = MultiSelectSpinnerAdapter(
+            this,
+            spinnerHobbyListItem!!,
+            selectedHobby
+        )
+
+        spinnerHobby?.adapter = adapter2
+
+        adapter2.setOnItemSelectedListener(object :
+            MultiSelectSpinnerAdapter.OnItemSelectedListener {
+            override fun onItemSelected(
+                selectedItems: List<ListItem>,
+                pos: Int,
+            ) {
+                nameHobby?.text = "Hobby"
+
+                Log.e("getSelectedItems", selectedItems.toString())
+                Log.e("getSelectedItems", selectedItems.size.toString())
+            }
+        }
+        )
+
+        val countries = resources.getStringArray(R.array.countries)
+        val arrayCountries = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            countries
+        )
+
+        binding.countryAutoComplete.setAdapter(arrayCountries)
+        binding.activitySpinner.setAdapter(adapter)
+        binding.hobbySpinner.setAdapter(adapter2)
 
         selectedDate = ""  // Initialize with an empty string
 
@@ -48,75 +124,54 @@ class AddHolidayActivity : AppCompatActivity() {
             }"
         }
 
-        val hobbies = resources.getStringArray(R.array.hobbys)
-        val activities = resources.getStringArray(R.array.activities)
-        val selectedHobbiesFlags = BooleanArray(hobbies.size)
-        val selectedActivitiesFlags = BooleanArray(activities.size)
-
-        hobbiesButton.setOnClickListener {
-            showMultiSelectDialog(hobbies, selectedHobbiesFlags, "Select Hobbies") { selected ->
-                selectedHobbies = selected.toMutableList()
-            }
-        }
-
-        activitiesButton.setOnClickListener {
-            showMultiSelectDialog(activities, selectedActivitiesFlags, "Select Activities") { selected ->
-                selectedActivities = selected.toMutableList()
-            }
-        }
-
         val saveButton: Button = findViewById(R.id.saveButton)
         saveButton.setOnClickListener {
             saveHolidayDetails()
         }
     }
 
-    private fun showMultiSelectDialog(items: Array<String>, selectedItems: BooleanArray, title: String, onSelectionChanged: (selected: List<String>) -> Unit) {
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle(title)
-        builder.setMultiChoiceItems(items, selectedItems) { _, which, isChecked ->
-            selectedItems[which] = isChecked
-        }
-        builder.setPositiveButton("OK") { dialog, _ ->
-            val selectedStrings = items.filterIndexed { index, _ -> selectedItems[index] }
-            onSelectionChanged(selectedStrings)
-            dialog.dismiss()
-        }
-        builder.setNegativeButton("Cancel") { dialog, _ ->
-            dialog.dismiss()
-        }
-        builder.create().show()
-    }
 
     private fun saveHolidayDetails() {
+        val country = binding.countryAutoComplete.text.toString()
+        val activity = selectedActivity?.map { it.name  to it.itemId}?.toMap()
+        val hobby = selectedHobby.map { it.name to it.itemId }.toMap()
+
+        val countries = resources.getStringArray(R.array.countries)
+
         val holidayName = holidayNameEditText.text.toString().trim()
-        val selectedCountry = countryAutoComplete.text.toString()
+        if (holidayName.isEmpty() || selectedDate.isEmpty() || country.isEmpty() || activity.isNullOrEmpty() || hobby.isNullOrEmpty()) {
+            if (holidayName.isEmpty())
+                Toast.makeText(this, "Holiday name cannot be empty!", Toast.LENGTH_LONG).show()
+            if (selectedDate.isEmpty())
+                Toast.makeText(this, "Please select a date!", Toast.LENGTH_LONG).show()
+            if (country.isEmpty()) binding.countryAutoComplete.error = "Choose a country!"
+            if (activity.isNullOrEmpty()) Toast.makeText(
+                this,
+                "Choose an activity!",
+                Toast.LENGTH_SHORT).show()
+            if (hobby.isNullOrEmpty()) Toast.makeText(
+                this,
+                "Choose a hobby!",
+                Toast.LENGTH_SHORT).show()
+        } else if (!countries.contains(country)) {
+            binding.countryAutoComplete.error = "No country specified in the database!"
+        } else {
+            val firebaseRef = FirebaseDatabase.getInstance().getReference("HolidayNames").child(selectedDate)
+            val countryData = "${countries.indexOf(country)+1}"
 
-        if (holidayName.isEmpty()) {
-            Toast.makeText(this, "Holiday name cannot be empty!", Toast.LENGTH_LONG).show()
-            return
-        } else if (selectedDate.isEmpty()) {
-            Toast.makeText(this, "Please select a date!", Toast.LENGTH_LONG).show()
-            return
-        }
-
-        val database = FirebaseDatabase.getInstance()
-        val holidayDetailsRef = database.getReference("HolidayNames").child(selectedDate)
-
-        val holidayDetails = mapOf(
-            "name" to holidayName,
-            "country" to selectedCountry,
-            "hobbies" to selectedHobbies.joinToString(", "),
-            "activities" to selectedActivities.joinToString(", ")
-        )
-
-        holidayDetailsRef.setValue(holidayDetails).addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                Toast.makeText(this, "Holiday details saved!", Toast.LENGTH_SHORT).show()
-                finish()
-            } else {
-                Toast.makeText(this, "Write error!", Toast.LENGTH_SHORT).show()
-            }
+            firebaseRef.child(selectedDate).child("Country").child(country).setValue(countryData)
+            firebaseRef.child(selectedDate).child("Activities").setValue(activity)
+            firebaseRef.child(selectedDate).child("Hobbies").setValue(hobby)
+            firebaseRef.child(selectedDate).child("name").setValue(holidayName)
+                .addOnCompleteListener {
+                    Toast.makeText(this, "Data changed successfully!", Toast.LENGTH_SHORT)
+                        .show()
+                    finish()
+                }
+                .addOnFailureListener { exception ->
+                    Toast.makeText(this, "Error: ${exception.message}", Toast.LENGTH_SHORT)
+                        .show()
+                }
         }
     }
 }
